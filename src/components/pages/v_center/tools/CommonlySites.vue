dceb4d85d25e57c5b880039d69cf05ccfc1849f4
adminlte <geforce96@hotmail.com>
2018-04-09 20:40:58 +0800

    change

--- src/components/pages/v_center/tools/CommonlySites.vue
+++ src/components/pages/v_center/tools/CommonlySites.vue
@@ -113,7 +113,7 @@
 </template>
 <script>
   import api from '@/api/api'
-  import ToolsMenu from '@/components/pages/v_center/ToolsMenu'
+  import ToolsMenu from '@/components/pages/v_center/tools/ToolsMenu'
   export default {
     name: 'commonlySites',
     components: {
