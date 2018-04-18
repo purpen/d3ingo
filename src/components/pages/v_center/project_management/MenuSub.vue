3c3a28872237b06e8bd7d44e84bc8f20bfca7dd9
adminlte <geforce96@hotmail.com>
2018-04-18 22:48:26 +0800

    添加项目设置

--- src/components/pages/v_center/project_management/MenuSub.vue
+++ src/components/pages/v_center/project_management/MenuSub.vue
@@ -21,52 +21,125 @@
       <router-link to="">项目需求</router-link>
       <router-link :to="{path: ''}">项目报价</router-link>
       <router-link :to="{path: ''}">合同</router-link>
-      <router-link :to="{path: ''}">菜单</router-link>
+      <a @click="cover = true">菜单</a>
     </div>
-    <section class="cover" v-if="show.cover">
+    <section class="cover" @click.self="cover = false" v-if="cover">
       <div class="cover-content">
         <div class="cover-header clear">项目设置
-          <span class="fr fx fx-icon-nothing-close-error"></span>
+          <span class="fr fx fx-icon-nothing-close-error" @click="cover = false"></span>
         </div>
         <div class="cover-body">
           <div class="cover-body-left">
-            <span>项目信息</span>
-            <span>客户信息</span>
-            <span>权限信息</span>
+            <span @click="changeOption('project')" :class="{'active': option === 'project'}">项目信息</span>
+            <span @click="changeOption('customer')" :class="{'active': option === 'customer'}">客户信息</span>
+            <span @click="changeOption('permission')" :class="{'active': option === 'permission'}" v-if="false">权限信息</span>
           </div>
-          <div class="cover-body-right">
+          <div class="cover-body-right scroll-bar" v-if="option === 'project'">
             <section>
               <h3>项目名称</h3>
-              <input type="text" v-model.trim="project.name">
+              <input placeholder="请填写项目名称"
+                type="text" v-model.trim="project.name">
             </section>
             <section class="flex-box">
-              <div>
+              <div class="flex1">
                 <h3>商务经理</h3>
-                <input type="text" v-model.trim="project.manager">
+                <input placeholder="请填写项目经理"
+                  type="text" v-model.trim="project.manager">
               </div>
-              <div>
+              <div class="flex1">
                 <h3>项目负责人</h3>
-                <input type="text" v-model.trim="project.leader">
+                <input placeholder="请填写项目负责人"
+                  type="text" v-model.trim="project.leader">
               </div>
             </section>
             <section class="flex-box">
-              <div>
+              <div class="flex1">
                 <h3>项目费用</h3>
-                <input type="text" v-model.trim="project.expense">
+                <input placeholder="请填写项目费用"
+                  type="text" v-model.trim="project.expense">
               </div>
-              <div>
+              <div class="flex1">
                 <h3>项目工作地点</h3>
-                <input type="text" v-model.trim="project.addr">
+                <input placeholder="请填写项目工作地点"
+                  type="text" v-model.trim="project.addr">
               </div>
             </section>
             <section>
               <h3>设计类别</h3>
-              <input type="text" v-model.trim="project.name">
+              <input placeholder="请填写设计类别"
+                type="text" v-model.trim="project.category">
             </section>
             <section>
               <h3>产品所属领域</h3>
-              <input type="text" v-model.trim="project.name">
+              <input placeholder="请填写产品所属领域"
+                type="text" v-model.trim="project.field">
             </section>
+            <section class="flex-box">
+              <div class="flex1">
+                <h3>投入时间</h3>
+                <el-input placeholder="请填写所需天数" v-model="project.time">
+                  <template slot="append">工作日</template>
+                </el-input>
+              </div>
+              <div class="flex1">
+                <h3>项目开始时间</h3>
+                <el-date-picker
+                  v-model="project.startTime"
+                  type="date"
+                  placeholder="请填写项目开始时间">
+                </el-date-picker>
+              </div>
+            </section>
+            <section>
+              <h3>项目等级</h3>
+              <el-select v-model="project.level" placeholder="请选择">
+                <el-option
+                  v-for="(item, index) in levels"
+                  :key="index"
+                  :label="item.label"
+                  :value="item.value">
+                  <span :style="{
+                    float: 'left',
+                    width: '10px',
+                    height: '10px',
+                    borderRadius: '50%',
+                    margin: '5px 10px 0 0',
+                    background: item.color}"></span>
+                  <span style="float: left">{{ item.label }}</span>
+                </el-option>
+              </el-select>
+            </section>
+            <div class="buttons clearfix">
+              <button class="fr middle-button full-red-button">保存</button>
+              <button class="fr middle-button white-button" @click="cover = false">取消</button>
+            </div>
+          </div>
+          <div class="cover-body-right cover-customer scroll-bar" v-if="option === 'customer'">
+            <section>
+              <h3>项目等级</h3>
+              <input type="text" placeholder="请输入客户名称或者选择已有客户">
+            </section>
+            <section class="flex-box">
+              <div class="flex1">
+                <h3>联系人</h3>
+                <input placeholder="请填写项目费用"
+                  type="text" v-model.trim="project.expense">
+              </div>
+              <div class="flex1">
+                <h3>职位电话</h3>
+                <input placeholder="请填写项目费用"
+                  type="text" v-model.trim="project.expense">
+              </div>
+              <div class="flex1">
+                <h3>电话</h3>
+                <input placeholder="请填写项目工作地点"
+                  type="text" v-model.trim="project.addr">
+              </div>
+            </section>
+            <div class="buttons clearfix">
+              <button class="fr middle-button full-red-button">保存</button>
+              <button class="fr middle-button white-button" @click="cover = false">取消</button>
+            </div>
           </div>
         </div>
       </div>
@@ -78,16 +151,45 @@ export default {
   name: 'projectManagementMenuSub',
   data() {
     return {
-      show: {
-        cover: true
-      },
+      cover: false,
+      option: 'project', // project customer permission
       project: {
         name: '',
         manager: '',
         leader: '',
         expense: '',
-        addr: ''
-      }
+        addr: '',
+        field: '',
+        category: '',
+        time: '',
+        startTime: '',
+        level: 1
+      },
+      customer: {
+        company_name: '',
+        contact_name: '',
+        position: '',
+        phone: '',
+        province: '',
+        city: '',
+        address: '',
+        user_id: ''
+      },
+      levels: [{
+        value: 1,
+        label: '普通',
+        color: '#999'
+      },
+      {
+        value: 2,
+        label: '紧急',
+        color: '#ffd330'
+      },
+      {
+        value: 3,
+        label: '非常紧急',
+        color: '#ff5a5f'
+      }]
     }
   },
   props: {
@@ -105,6 +207,11 @@ export default {
   computed: {
     routeId() {
       return this.$route.params.id
+    }
+  },
+  methods: {
+    changeOption(e) {
+      this.option = e
     }
   }
 }
@@ -129,6 +236,7 @@ header {
 .pm-left a,
 .pm-middle a,
 .pm-right a {
+  cursor: pointer;
   font-size: 16px;
   color: #666;
   position: relative;
@@ -211,7 +319,7 @@ header {
   background: #fff;
   margin: 48px auto;
   width: 800px;
-  min-height: 600px;
+  max-height: 650px;
 }
 .cover-header {
   flex: 0 1 60px;
@@ -248,7 +356,8 @@ header {
   font-size: 14px;
   color: #666;
   height: 50px;
-  border-bottom: 1px solid #d2d2d2
+  border-bottom: 1px solid #d2d2d2;
+  border-left: 3px solid transparent
 }
 .cover-body-left span:hover {
   color: #ff5a5f;
@@ -260,8 +369,46 @@ header {
 .cover-body-right {
   flex: 1 1 auto;
   padding: 30px;
+  overflow: auto;
+  position: relative;
 }
 .flex-box {
   display: flex
+}
+.flex-box .flex1 {
+  flex: 1
+}
+.flex-box .flex1:first-child {
+  margin-right: 20px;
+}
+.flex-box .flex1:nth-child(3){
+  margin-left: 20px;
+}
+.cover-body-right section {
+  padding-bottom: 20px;
+}
+.cover-body-right section h3 {
+  font-size: 14px;
+  padding-bottom: 10px;
+  color: #222
+}
+.cover-body-right section input {
+  padding: 0 8px;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #d2d2d2;
+  border-radius: 4px;
+  font-size: 14px;
+}
+.buttons button:last-child {
+  margin-right: 20px;
+}
+.cover-customer .buttons {
+  position: absolute;
+  left: 0;
+  bottom: 30px;
+  width: 100%;
+  padding: 0 30px;
 }
 </style>
