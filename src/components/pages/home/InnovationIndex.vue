049fff37c2694d44995528671fefa0fd4dd76066
adminlte <geforce96@hotmail.com>
2018-05-08 00:14:45 +0800

    add image

--- src/components/pages/home/InnovationIndex.vue
+++ src/components/pages/home/InnovationIndex.vue
@@ -4,19 +4,78 @@
     </div>
     <div class="giftbody">
       <a v-if="!isLogin" @click="upload" class="add-work">加入铟果</a>
-      <h2><span class="icon">铟果D³INGO Index指数说明</span></h2>
+      <h2><span class="icon">综述</span></h2>
       <p class="padding26">太火鸟·铟果D³INGO Index“中国设计企业创新力指数”是全球首个基于人工智能的设计创新企业综合实力指数排行榜，由太火鸟科技旗下的产品创新引擎——铟果D³INGO（简称铟果）于2018年初正式推出。</p>
 
-      <p class="padding26">太火鸟·铟果D³INGO 作为中国领先的产品创新引擎以及智能分发平台，以中国创新产品策源地为愿景，致力于打通设计创新、优质制造、消费零售为一体的全新创新价值链，在消费升级的大时代趋势下助力我国传统产业高效转型。</p>
+      <p class="padding26">如今，设计创新与科技创新与成为驱动全球经济发展的新主流，“设计创新”更是决胜全人类的未来的战略关键。基于此时代背景， 太火鸟·铟果D³IN GO整合设计创意产业、尤其是工业设计领域各环节资源，运用人工智能技术、大数据、云计算及深度学习算法，自主研发推出核心指数算法，对全国数千家设计机构的企业规模、市场份额、获奖数量、品牌影响等数据进行深度筛选、分析，划分为基础运作力、商业决策力、创新交付力、品牌溢价力、客观公信力、风险应激力六大评级维度，配合其他数据进行分析和科学排序，以季度为单位，生成最能够体现中国设计创新机构综合实力贺发展趋势的综合性指数榜单，为设计行业的从业者、研究者以及关注者提供权威参考。</p>
 
-      <p class="padding26">如今，设计创新与科技创新与成为驱动全球经济发展的新主流，“设计创新”更是决胜全人类的未来的战略关键。基于此时代背景，太火鸟旗下“D³IN铟立方未来实验室”历时一年，运用人工智能技术、大数据、云计算及深度学习算法，自主研发推出了核心指数算法，形成了太火鸟·铟果D³INGO Index“中国设计企业创新力指数”排行产品。</p>
+      <p class="padding26">太火鸟科技作为中国领先的产品创新引擎以及智能分发平台，以中国创新产品策源地为愿景，致力于打通设计创新、优质制造、消费零售为一体的全新创新价值链，在消费升级的大时代趋势下助力我国传统产业高效转型。</p>
 
-      <p>太火鸟·铟果D³INGO Index“中国设计企业创新力指数”整合了设计创意产业各环节资源，对全国数千家设计机构的企业规模、市场份额、获奖数量、品牌影响等数据进行深度筛选、分析，划分为基础能力、创新能力、设计能力、商业能力、社会信誉、行业影响力六大评级维度，以季度为单位，生成最能够体现中国设计创新机构综合实力的综合性指数榜单。</p>
+      <h2><span class="icon">六大维度解析</span></h2>
+      <ul class="dimension">
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/basic_power@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>基础运作力</p>
+            <p>BASIC POWER</p>
+          </div>
+          <div class="dimension-content">该维度主要体现个体机构在创新发力基础、可持续创新条件、客观硬件能力、自主变革基因、原始驱动力等方面的能力</div>
+        </li>
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/innovate@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>创新交付力</p>
+            <p>INNOVATION DELIVERY PERFORMANCE</p>
+          </div>
+          <div class="dimension-content">该维度集中体现个体在前瞻敏感度、商业本质抓取与回归能力、商业环境下的参与感、趋势干预能力、竞品博弈能力等与创新密切相关方面的表现</div>
+        </li>
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/business_making_power@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>商业决策力</p>
+            <p>BUSINESS DECISION-MAKING POWER</p>
+          </div>
+          <div class="dimension-content">该维度集中体现个体在现实需求的挖掘与转化能力，创意执行力度、交付诚意与专业素养等与具体设计项目相关环节的实力</div>
+        </li>
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/business@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>品牌溢价力</p>
+            <p>BRAND PREMIUM POTENTIAL</p>
+          </div>
+          <div class="dimension-content">该维度侧重个体的品牌化实力，参考数据包括但不仅限于个体的品牌养成能力，创始人品牌观，商业语言中的品牌价值……</div>
+        </li>
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/objective_credibility@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>客观公信力</p>
+            <p>OBJECTIVE CREDIBILITY</p>
+          </div>
+          <div class="dimension-content">该维度主要指个体的媒体影响力、社会影响力、行业口碑、社会影响力等等</div>
+        </li>
+        <li class="dimension-resolve">
+          <div class="img-box">
+            <img :src="require('assets/images/subject/innovation/risk_response_ability@2x.png')" alt="">
+          </div>
+          <div class="dimension-title">
+            <p>风险应激力</p>
+            <p>RISK RESPONSE ABILITY</p>
+          </div>
+          <div class="dimension-content">该维度主要考量个体在真实商业环境下的抗风险情况，包括但不仅限于新生态适应力、抗风险能力、应变能力、变革生存值……</div>
+        </li>
+      </ul>
 
-      <h2><span class="icon">铟果D³INGO Index指数排行目标</span></h2>
-      <p class="text-center">设计创新领域，主要是工业设计与交互设计领域的所有从业机构。</p>
-
-      <h2><span class="icon">铟果D³INGO Index指数算法说明</span></h2>
+      <h2><span class="icon">太火鸟•铟果D³INGO中国设计企业创新指数算法说明</span></h2>
       <h3 class="first-icon-dot"><span class="icon-dot">数据来源</span></h3>
       <p class="data-origin"><i></i><span>设计机构在铟果D³INGO产品创新SaaS平台提交的真实有效数据；</span>
       <p class="data-origin"><i></i><span>设计企业官方网站、微信公众号，以及百度指数、微信指数等互联网开放数据；</span></p>
@@ -195,7 +254,7 @@ export default {
 .icon:before {
   content: '';
   position: absolute;
-  left: -30px;
+  left: -40px;
   top: 50%;
   margin-top: -10px;
   width: 20px;
@@ -209,7 +268,7 @@ export default {
 .icon:after {
   content: '';
   position: absolute;
-  right: -30px;
+  right: -40px;
   top: 50%;
   margin-top: -10px;
   width: 20px;
@@ -223,7 +282,7 @@ export default {
 .icon-dot:before {
   content: '';
   position: absolute;
-  left: -20px;
+  left: -30px;
   top: 50%;
   margin-top: -5px;
   width: 10px;
@@ -237,7 +296,7 @@ export default {
 .icon-dot:after {
   content: '';
   position: absolute;
-  right: -20px;
+  right: -30px;
   top: 50%;
   margin-top: -4px;
   width: 10px;
@@ -277,7 +336,37 @@ export default {
   border: 5px solid transparent;
   border-left: 10px solid #F9DA58;
 }
+.dimension {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  flex-wrap: wrap;
+}
+.dimension-resolve {
+  width: 33%;
+  padding: 0 30px 50px;
+}
+.dimension-resolve .img-box {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20px;
+}
 
+.dimension-resolve .img-box img {
+  width: 70px;
+  height: 70px;
+}
+.dimension-title {
+  text-align: center;
+  font-size: 14px;
+  color: #222;
+}
+.dimension-content {
+  color: #666;
+  line-height: 1.5;
+  font-size: 12px;
+}
 @media screen and (max-width: 767px) {
   .add-work {
     position: relative;
