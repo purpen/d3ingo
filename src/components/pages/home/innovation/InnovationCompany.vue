02818f2922efac112ecdf3bb5c68498e50bafbe2
adminlte <geforce96@hotmail.com>
2018-05-09 10:56:40 +0800

    引入vue-echarts

--- src/components/pages/home/innovation/InnovationCompany.vue
+++ src/components/pages/home/innovation/InnovationCompany.vue
@@ -39,21 +39,7 @@
 </template>
 <script>
 import api from '@/api/api'
-import ECharts from 'vue-echarts/components/ECharts.vue'
-import 'echarts/lib/chart/bar'
-import 'echarts/lib/chart/line'
-import 'echarts/lib/chart/pie'
-import 'echarts/lib/chart/map'
-import 'echarts/lib/chart/radar'
-import 'echarts/lib/chart/scatter'
-import 'echarts/lib/chart/effectScatter'
-import 'echarts/lib/component/tooltip'
-import 'echarts/lib/component/polar'
-import 'echarts/lib/component/geo'
-import 'echarts/lib/component/legend'
-import 'echarts/lib/component/title'
-import 'echarts/lib/component/visualMap'
-import 'echarts/lib/component/dataset'
+import ECharts from 'vue-echarts'
 export default {
   name: 'innavationCompany',
   data() {
@@ -69,7 +55,7 @@ export default {
       designCaseList: [],
       option: {
         title: {
-          text: '创新力雷达图'
+          // text: '创新力雷达图'
         },
         tooltip: {},
         radar: {
