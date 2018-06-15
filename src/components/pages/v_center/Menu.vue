ef11083f41434668b585cc323c1fb2a1de964b61
adminlte <geforce96@hotmail.com>
2018-06-15 14:30:16 +0800

    fix

--- src/components/pages/v_center/Menu.vue
+++ src/components/pages/v_center/Menu.vue
@@ -68,6 +68,12 @@
                 控制面板
               </a>
             </el-tooltip>
+            <el-tooltip v-if="true" class="item" :effect="DarkorLight" content="项目管理" placement="right">
+            <a @click="alick" :to="'/vcenter/project_management/list'"
+              :class="['item', 'management', {'is-active': currentName === 'project_management'}]">
+              项目管理
+            </a>
+            </el-tooltip>
             <el-tooltip class="item" :effect="DarkorLight" content="项目云盘" placement="right">
             <a @click="alick" :to="'/vcenter/cloud_drive/list/all'"
               :class="['item', 'cloud', {'is-active': currentName === 'cloud_drive'}]">
@@ -85,12 +91,6 @@
             <a @click="alick" :to="'/vcenter/account/base'"
               :class="['item', 'account-management', {'is-active': currentName === 'profile'}]">
               账号设置
-            </a>
-            </el-tooltip>
-            <el-tooltip v-if="true" class="item" :effect="DarkorLight" content="项目管理" placement="right">
-            <a @click="alick" :to="'/vcenter/project_management/list'"
-              :class="['item', 'management', {'is-active': currentName === 'project_management'}]">
-              项目管理
             </a>
             </el-tooltip>
             <el-tooltip class="item" :effect="DarkorLight" content="查看公司主页" placement="right">
@@ -217,10 +217,14 @@
       <section :class="['menuHide', 'scroll-bar', {'MmenuHide': isMob, 'menuHide-mini': leftWidth === 2}]">
         <div v-if="isCompany">
           <div :class="['menu-list', 'clearfix', {'Mmenulist': isMob, }]" ref="Mmenulist" v-if="isChild">
-              <a @click="alick" :to="'/vcenter/child_control'"
-                :class="['item', 'dashboard', {'is-active': currentName === 'control'}]">
-                控制面板
-              </a>
+            <a @click="alick" :to="'/vcenter/child_control'"
+              :class="['item', 'dashboard', {'is-active': currentName === 'control'}]">
+              控制面板
+            </a>
+            <a @click="alick" :to="'/vcenter/project_management/list'"
+              :class="['item', 'management', {'is-active': currentName === 'project_management'}]">
+              项目管理
+            </a>
             <a @click="alick" :to="'/vcenter/cloud_drive/list/all'"
               :class="['item', 'cloud', {'is-active': currentName === 'cloud_drive'}]">
               项目云盘
@@ -233,10 +237,6 @@
             <a @click="alick" :to="'/vcenter/account/base'"
               :class="['item', 'account-management', {'is-active': currentName === 'profile'}]">
               账号设置
-            </a>
-            <a @click="alick" :to="'/vcenter/project_management/list'"
-              :class="['item', 'management', {'is-active': currentName === 'project_management'}]">
-              项目管理
             </a>
             <a :class="['item', {'is-active': currentName === 'company'}]" @click="redirectCompany" 
                v-if="isMob">
