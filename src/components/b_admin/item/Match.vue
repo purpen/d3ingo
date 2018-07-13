7a539678088471dec2a2c5c82696c3af3763625a
cutzhao <1759767628@qq.com>
2018-07-13 11:12:01 +0800

    后台提出

--- src/components/b_admin/item/Match.vue
+++ src/components/b_admin/item/Match.vue
@@ -16,7 +16,7 @@
 
 <script>
 import api from '@/api/api'
-import vMenu from '@/components/admin/Menu'
+import vMenu from '@/components/b_admin/Menu'
 export default {
   name: 'admin_item_match',
   components: {
