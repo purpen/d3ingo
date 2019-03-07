44f3f8bb0d1b72234ca1ac7c64e542d5cb969d3a
dingjiafan <386086288@qq.com>
2019-03-07 09:43:01 +0800

    保存

--- src/components/block/Footer.vue
+++ src/components/block/Footer.vue
@@ -7,7 +7,7 @@
           <router-link :to="{name: 'home'}">首页</router-link>
         </p>
         <p>
-          <router-link :to="{name: 'contact'}">设计案例</router-link>
+          <router-link :to="{name: 'design_case'}">设计案例</router-link>
         </p>
         <p>
           <router-link v-if="!isMob" :to="{name: 'spreadDesign'}">设计服务商入驻</router-link>
