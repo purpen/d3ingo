2b40e5e1580ce5e7eb92e33d73ae70f0627cb5a0
dingjiafan <386086288@qq.com>
2019-04-19 11:47:44 +0800

    详情页

--- src/components/pages/design_case/Show.vue
+++ src/components/pages/design_case/Show.vue
@@ -1,252 +1,83 @@
 <template>
-  <div class="container" v-loading.fullscreen.lock="isFullLoading">
-    <div class="blank20"></div>
-    <el-row :gutter="20" class="anli-elrow">
-
-      <el-col :xs="24" :sm="18" :md="18" :lg="18">
-        <div class="design-case-content edit-content">
-          <div class="title">
-            <h1>{{ item.title }}</h1>
-          </div>
-          <div class="summary">
-            <p>{{ item.profile }}</p>
-          </div>
-          <div class="des">
-            <p v-for="(d, index) in item.case_image" :key="index">
-              <img :src="d.big" :alt="d.name" :title="d.name"/>
-              <slot>
-            <p class="img-des">{{ d.summary }}</p>
-            </slot>
-            </p>
-          </div>
-        </div>
-      </el-col>
-
-      <el-col :xs="24" :sm="6" :md="6" :lg="6">
-        <div class="design-case-slide">
-          <div class="info">
-            <router-link :to="{name: 'companyShow', params: {id: item.design_company.id}}" target="_blank">
-              <img class="avatar" v-if="item.design_company.logo_url" :src="item.design_company.logo_url" width="100"/>
-              <img class="avatar" v-else src="../../../assets/images/avatar_100.png" width="100"/>
-            </router-link>
-            <h3 class="pointer" @click="goCompany(item.design_company.id)">{{ item.design_company.company_name }}</h3>
-            <p class="com-addr"><i class="fx-icon-location"></i><span>{{ item.design_company.province_value }}</span><span>{{ item.design_company.city_value }}</span>
-            </p>
-          </div>
-          <div class="rate">
-            <p>信用指数：
-              <span>{{ item.design_company.score }}分</span>
-            </p>
-          </div>
-          <div class="cate" v-if="item.design_type_val">
-            <p class="c-title">类别</p>
-            <p class="tag">
-              <span>{{ item.design_type_val }}</span>
-            </p>
-          </div>
-
-          <div class="cate" v-if="item.field_val">
-            <p class="c-title">领域</p>
-            <p class="tag">
-              <span>{{ item.field_val }}</span>
-            </p>
-          </div>
-
-          <div class="cate" v-if="item.industry_val">
-            <p class="c-title">行业</p>
-            <p class="tag">
-              <span>{{ item.industry_val }}</span>
-            </p>
-          </div>
-
-          <div class="cate" v-if="item.prize_val">
-            <p class="c-title">获得奖项</p>
-            <p class="tag">
-              <span>{{ item.prize_val }}</span>
-            </p>
-          </div>
-
-        </div>
-      </el-col>
-
-    </el-row>
-
-  </div>
+  <section class="round">
+    <head class="header flex-column-center">
+      <div class="banner">
+        <img src="">
+      </div>
+      <h1>电动VR摇臂</h1>
+      <section class="">
+        <article>
+          <div class="img-desigin"></div>
+          <p>产品设计 / 产品外观设计</p>
+        </article>
+        <article>
+          <div class="img-date"></div>
+          <p>2019.3.1 17:50:39</p>
+        </article>
+      </section>
+    </head>
+    <section class="center">
+      <div class="share">
+        <div class="share-text"></div>
+        <div class="share-wx"></div>
+      </div>
+      <article>
+        <p>普通及虚拟演播室双用途拍摄设备；可自动识别位置镜头电机；双高清大屏显示；依使用环境臂身前后可移动，臂身高度可调；新材料应用，质量更轻，画面更稳定；XYZ三轴摄像机画面运动，画面语言更丰富。能实现普通电视画面录制，以及虚拟图像跟踪画面录制。臂身可改造成4-12米，高度可调节1.6-1.8米。双屏显示人机互动，数据直观。操作稳定灵活。VR画面录制省去搭建实景舞台及装饰的耗时耗人工的弊端，由电脑三维动画与主持人画面对接完成，跟踪准确。漆面采用无害硬氧处理，电路处理安全，系统能耗不超50W。人机互动操作台：触屏面板，阻尼俯仰，多页参数显示；新材料应用：轻量化设计，负载更轻，适用更灵活；人性化设计；人体力学应用，操作更方便；手感轻盈，运动指令性更强；结构考究。</p>
+      </article>
+      <article>
+        <img src="../../../assets/images/icon/jd_icon.png">
+      </article>
+    </section>
+    <footer></footer>
+  </section>
 </template>
-
 <script>
-import api from '@/api/api'
-import '@/assets/js/format'
 export default {
-  name: 'design_case_show',
-  data() {
-    return {
-      isFullLoading: false,
-      item: {
-        design_company: ''
-      },
-      rateValue: 3.5,
-      msg: 'This is About!!!'
-    }
-  },
-  created: function() {
-    let id = this.$route.params.id
-    const that = this
-    that.isFullLoading = true
-    that.$http
-      .get(api.designCaseId.format(id), {})
-      .then(function(response) {
-        that.isFullLoading = false
-        if (response.data.meta.status_code === 200) {
-          that.item = response.data.data
-          if (that.item.design_company.logo_image) {
-            that.item.design_company.logo_url =
-              that.item.design_company.logo_image.logo
-          } else {
-            that.item.design_company.logo_url = false
-          }
-          document.title = that.item.title + '-铟果'
-        } else {
-          that.$message.error(response.data.meta.message)
-        }
-      })
-      .catch(function(error) {
-        that.$message.error(error.message)
-        that.isFullLoading = false
-      })
-  },
-  methods: {
-    goCompany(id) {
-      this.$router.push({name: 'companyShow', params: {id: id}})
-    }
-  }
+  name: 'design_case_show'
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="stylus" scoped>
-.design-case-content {
-  padding: 20px 20px 20px 20px;
+<style>
+.round {
+  background-color: #F7F7F7;
 }
-
-.design-case-content .title {
-  text-align: center;
-  color: #222;
-  margin: 20px 0 20px 0;
-  font-size: 2rem;
-}
-
-.design-case-content .summary {
-  margin: 0 0 20px 0;
-}
-
-.design-case-content .summary p {
-  line-height: 1.6;
-  color: #666;
-}
-
-.design-case-content .des {
-}
-
-.design-case-content .des p {
-  line-height: 1.6;
-  text-align: center;
-}
-
-.design-case-slide {
-  padding: 20px;
-  color: #222;
-}
-
-.design-case-slide .info {
-  margin: 10px;
-  margin-bottom: 0;
-  text-align: center;
-}
-
-.design-case-slide .info h3 {
-  color: #222;
-}
-
-.design-case-slide .info p {
-  color: #222;
-}
-
-.info p span, .info p i {
-  margin-right: 10px
-}
-
-.design-case-slide .info img {
-  border-radius: 50%;
+.header {
+  height: 400px;
   overflow: hidden;
-  vertical-align: middle;
-  border: 1px solid #c8c8c8;
+  position: relative;
+}
+.banner {
+  position: absolute;
+}
+.center {
+  width: 90%;
+  max-width: 1180px;
+  background-color: #fff;
+}
+.share {
+  display: flex;
+}
+.share-text {
+  height: 40px;
+  width: 40px;
+  background: url('../../../assets/images/design-case/share@2x.png')
+}
+.share-wx {
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  background: url('../../../assets/images/design-case/WeChat02@2x.png')
+}
+.share-wx {
+  cursor: pointer;
+  height: 40px;
+  width: 40px;
+  background: url('../../../assets/images/design-case/WeChat02@2x.png')
 }
 
-.design-case-slide h3 {
-  margin-top: 10px;
-  font-size: 1.8rem;
-  line-height: 2.8rem;
-}
-
-.design-case-slide .com-addr {
-  line-height: 2.8rem;
-  color: #666;
-}
-
-.design-case-slide .rate {
-  padding: 10px;
-  text-align: center;
-}
-
-.design-case-slide .cate {
-  margin-top: 20px;
-}
-
-.cate p {
-  color: #666;
-}
-
-.cate p.c-title {
-  text-align: left;
-  font-size: 1.6rem;
-  font-weight: 500;
-  color: #222222;
-  margin-bottom: 5px;
-}
-
-.cate p.tag span {
-  color: #666666;
-}
-
-.design-case-slide .prize {
-  margin-top: 20px;
-}
-
-.edit-content {
-  padding: 20px;
-  overflow: hidden;
-}
-
-.edit-content img {
-}
-
-p.img-des {
-  margin-bottom: 20px;
-}
-
-@media screen and (max-width: 767px) {
-  .cate p {
-    text-align: center;
-  }
-
-  .cate p.c-title {
-    text-align: center;
-  }
-}
-@media screen and ( max-width: 480px) {
-  .container {
-    overflow-x: hidden;
-  }
+.flex-column-center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
