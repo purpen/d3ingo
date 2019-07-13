b47facfaafad9731fb515ee47737cd9f6476af7f
adminlte <geforce96@hotmail.com>
2019-07-13 14:21:57 +0800

    SnToken

---
+++ src/components/pages/auth/SnToken.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 class="tc-2 fz-20 text-center">{{token}}</h1>
+</template>
+<script>
+export default {
+  name: 'SnToken',
+  data() {
+    return {
+      token: ''
+    }
+  },
+  created() {
+    this.token = this.$route.query.token
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
