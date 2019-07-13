16a5fe32581f6b259de9aacf0bad2acc8443afb6
adminlte <geforce96@hotmail.com>
2019-07-13 15:54:45 +0800

    fix account

--- src/components/pages/projects/Release.vue
+++ src/components/pages/projects/Release.vue
@@ -400,8 +400,12 @@ export default {
       })
     },
     fetchCode3() {
-      if (!this.form3.account) {
-        this.$message.error('请输入手机号')
+      if (this.form3.account.length !== 11 || !/^((13|14|15|16|17|18|19)[0-9]{1}\d{8})$/.test(this.form3.account)) {
+        this.$message({
+          message: '手机号格式不正确!',
+          type: 'error',
+          duration: 1000
+        })
         return
       }
       if (this.isGettingCode) {
--- src/components/pages/promote/Promote3.vue
+++ src/components/pages/promote/Promote3.vue
@@ -343,8 +343,12 @@
       },
       // 点击获取验证码
       fetchCode() {
-        if (!this.form.account) {
-          this.$message.error('请输入手机号')
+        if (this.form.account.length !== 11 || !/^((13|14|15|16|17|18|19)[0-9]{1}\d{8})$/.test(this.form.account)) {
+          this.$message({
+            message: '手机号格式不正确!',
+            type: 'error',
+            duration: 1000
+          })
           return
         }
         this.$http.post(api.fetch_wx_code, {phone: this.form.account})
