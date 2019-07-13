4c7f1909d9a49eab815e943cf466ea6cb69e0fe1
adminlte <geforce96@hotmail.com>
2019-07-13 13:14:29 +0800

    fix 合同

--- src/components/pages/v_center/contract/snContract/SnDemandDown.vue
+++ src/components/pages/v_center/contract/snContract/SnDemandDown.vue
@@ -25,6 +25,37 @@
       }
     },
     methods: {
+      selectContract(item) { // 默认0 艺火1 义乌2 神农4
+        let source = item.source
+        let uType = this.$store.state.event.user.type
+        let uniqueId = this.$route.params.unique_id
+        if (item.version === 0) {
+          this.$router.push({name: 'vcenterContractDown0', query: {unique_id: uniqueId}})
+          return false
+        }
+        switch (source) {
+          case 1:
+            if (uType === 2) {
+              this.$router.replace({name: 'vcenterContractJdDesignDown', params: {unique_id: uniqueId}})
+            } else {
+              this.$router.replace({name: 'vcenterContractJdDemandDown', params: {unique_id: uniqueId}})
+            }
+            return false
+          case 2:
+            this.$router.push({name: '', params: {unique_id: uniqueId}})
+            return false
+          case 4:
+            if (uType === 2) {
+              this.$router.replace({name: 'vcenterContractSnDesignDown', params: {unique_id: uniqueId}})
+            } else {
+              this.$router.replace({name: 'vcenterContractSnDemandDown', params: {unique_id: uniqueId}})
+            }
+            return false
+          default:
+            this.$router.push({name: '', params: {unique_id: uniqueId}})
+            return false
+        }
+      },
       // 下载
       downBtn() {
         let stages = []
@@ -344,21 +375,22 @@
             if (response.data.meta.status_code === 200) {
               let item = response.data.data
               if (item) {
-                if (item.version === 0) {
-                  that.$router.push({name: 'vcenterContractDown0', query: {unique_id: uniqueId}})
-                  return false
-                }
-                // // 是否来源京东
-                if (item.source === 1) {
-                  let uType = that.$store.state.event.user.type
-                  // 如果是设计服务商
-                  if (uType === 2) {
-                    that.$router.replace({name: 'vcenterContractJdDesignDown', params: {unique_id: uniqueId}})
-                  } else {
-                    that.$router.replace({name: 'vcenterContractJdDemandDown', params: {unique_id: uniqueId}})
-                  }
-                  return
-                }
+                // if (item.version === 0) {
+                //   that.$router.push({name: 'vcenterContractDown0', query: {unique_id: uniqueId}})
+                //   return false
+                // }
+                // // // 是否来源京东
+                // if (item.source === 1) {
+                //   let uType = that.$store.state.event.user.type
+                //   // 如果是设计服务商
+                //   if (uType === 2) {
+                //     that.$router.replace({name: 'vcenterContractJdDesignDown', params: {unique_id: uniqueId}})
+                //   } else {
+                //     that.$router.replace({name: 'vcenterContractJdDemandDown', params: {unique_id: uniqueId}})
+                //   }
+                //   return
+                // }
+                that.selectContract(item)
                 item.stages = []
                 if (!item.demand_pay_limit) {
                   item.demand_pay_limit = that.contractScale.demand_pay_limit
--- src/components/pages/v_center/contract/snContract/SnDemandView.vue
+++ src/components/pages/v_center/contract/snContract/SnDemandView.vue
@@ -214,6 +214,37 @@
       }
     },
     methods: {
+      selectContract(item) { // 默认0 艺火1 义乌2 神农4
+        let source = item.source
+        let uType = this.$store.state.event.user.type
+        let uniqueId = this.$route.params.unique_id
+        if (item.version === 0) {
+          this.$router.push({name: 'vcenterContractView0', query: {unique_id: uniqueId}})
+          return false
+        }
+        switch (source) {
+          case 1:
+            if (uType === 2) {
+              this.$router.replace({name: 'vcenterContractJdDesignView', params: {unique_id: uniqueId}})
+            } else {
+              this.$router.replace({name: 'vcenterContractJdDemandView', params: {unique_id: uniqueId}})
+            }
+            return false
+          case 2:
+            this.$router.push({name: '', params: {unique_id: uniqueId}})
+            return false
+          case 4:
+            if (uType === 2) {
+              this.$router.replace({name: 'vcenterContractSnDesignView', params: {unique_id: uniqueId}})
+            } else {
+              this.$router.replace({name: 'vcenterContractSnDemandView', params: {unique_id: uniqueId}})
+            }
+            return false
+          default:
+            this.$router.push({name: '', params: {unique_id: uniqueId}})
+            return false
+        }
+      },
       // 同意合同
       agreeBtn() {
         this.sureDialogMessage = '本合同具有法律效力，确认合同后将按照合同内容付款并开始项目，请确保熟知合同内容。'
@@ -267,15 +298,16 @@
             if (response.data.meta.status_code === 200) {
               let item = response.data.data
               if (item) {
-                if (item.version === 0) {
-                  that.$router.push({name: 'vcenterContractView0', query: {unique_id: uniqueId}})
-                  return false
-                }
-                // 是否来源铟果
-                if (item.source === 0) {
-                  that.$router.replace({name: 'vcenterContractView', params: {unique_id: uniqueId}})
-                  return
-                }
+                // if (item.version === 0) {
+                //   that.$router.push({name: 'vcenterContractView0', query: {unique_id: uniqueId}})
+                //   return false
+                // }
+                // // 是否来源铟果
+                // if (item.source === 0) {
+                //   that.$router.replace({name: 'vcenterContractView', params: {unique_id: uniqueId}})
+                //   return
+                // }
+                that.selectContract(item)
                 that.itemId = item.id
                 that.itemName = item.title + '合同'
                 item.stages = []
