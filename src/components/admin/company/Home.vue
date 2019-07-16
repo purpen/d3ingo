c0e3696675f7cb52c784e054aa7b2bb241ed0e1a
qxx <861305133@qq.com>
2019-07-16 16:46:39 +0800

    是否有列表

--- src/components/admin/company/Home.vue
+++ src/components/admin/company/Home.vue
@@ -94,7 +94,7 @@
             <input placeholder="在当前列表下搜索" class="sever-right-select" v-model="seleValue" @keyup.enter="selectLoad()"/>
             <div class="select-img" @click="selectLoad()"></div>
           </div>
-          <div class="dot">
+          <div class="dot" v-if="tableData.length > 0">
             <div class="dot-hover">
               <div class="dot-flex" @click="downloads()">
                 <i class="el-icon-download dot-img1"></i>
